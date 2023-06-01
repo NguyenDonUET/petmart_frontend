31ac29bc8dbda04fcbc5ae3365c490a78850e3de
NguyenDonUET <nguyendoncb@gmail.com>
2023-06-02 00:53:44 +0700

    update responsive profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -477,8 +477,9 @@ const ProfilePage = () => {
                     templateColumns={{
                       base: "repeat(1, 1fr)",
                       sm: "repeat(2, 1fr)",
-                      md: "repeat(3, 1fr)",
-                      lg: "repeat(4, 1fr)",
+                      md: "repeat(2, 1fr)",
+                      lg: "repeat(3, 1fr)",
+                      xl: "repeat(5, 1fr)",
                     }}
                     gap={1}
                   >
