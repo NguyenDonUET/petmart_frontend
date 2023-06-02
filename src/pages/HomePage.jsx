1b11f4342266a066eac3701e45e8936d5742104b
NguyenDonUET <nguyendoncb@gmail.com>
2023-06-02 08:27:20 +0700

    Xử lý khi token hết hạn ở home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,31 @@
-import { lazy } from "react";
+import { lazy, useEffect } from "react";
 import Hero from "../layouts/Hero";
-import { Box } from "@chakra-ui/react";
+import { Box, useToast } from "@chakra-ui/react";
 import Posts from "../layouts/Posts";
 import LazyLoadingContainer from "../components/LazyLoadingContainer";
+import { useSelector } from "react-redux";
+import { isTokenExpired } from "../redux/auth/auth";
+import { useNavigate } from "react-router-dom";
 const LazyPosts = lazy(() => import("../layouts/Posts"));
 
 const HomePage = () => {
+  const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const toast = useToast();
+  const { userInfo } = user;
+
+  useEffect(() => {
+    if (userInfo && isTokenExpired(userInfo.accessToken)) {
+      toast({
+        description: "Phiên làm việc đã hêt hạn vui lòng đăng nhập lại",
+        status: "error",
+        isClosable: true,
+        position: "top",
+      });
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <Box as="main">
       <Hero />
